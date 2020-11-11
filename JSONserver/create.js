330950e6910b0d6fab281e76ca8d28ce5044c4d2
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-11-11 21:48:35 +0900

    feat: post create

--- JSONserver/create.js
+++ JSONserver/create.js
@@ -2,17 +2,17 @@ const createBtn = document.querySelector('.create-post');
 
 createBtn.addEventListener('click', async () => {
   const doc = {
-    id: 7,
     title: 'aaa',
     author: 'fdsfsd',
-    likes: 312,
-    comments: 2,
+    likes: 0,
+    comments: 0,
   };
 
+  console.log(doc)
   await fetch('http://localhost:3000/posts', {
-    method: 'post',
+    method: 'POST',
     body: JSON.stringify(doc),
-    headers: { 'Contnt-Type': 'application/json' },
+    headers:{'Content-Type' : 'application/json'}
   });
 });
 
